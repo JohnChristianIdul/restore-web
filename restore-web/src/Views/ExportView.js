ff65993ab458b3333b9da8f84cb9d640815d49a0
John Christian Idul <idulchristian@gmail.com>
2024-05-06 08:01:08 +0800

    Models and Views Done, but has not been tested yet

---
+++ restore-web/src/Views/ExportView.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Button} from '@mui/material';
+
+const ExportView = () => {
+    return (
+        <div>
+            <Button variant="contained" color="primary">
+                Export
+            </Button>
+        </div>
+    )
+}
+
+export default ExportView;
