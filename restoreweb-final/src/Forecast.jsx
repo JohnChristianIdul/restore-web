ff1b910a49ed7113073b215ce9afa24503f89f79
JohnChristianIdul <idulchristian@gmail.com>
2024-08-08 11:47:36 +0800

    Add Forecast file and modify App and Forecast

---
+++ restoreweb-final/src/Forecast.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Header from './views/Header.jsx';
+import './index.css';
+import ForecastCardController from "./controller/ForecastCardController.jsx";
+import NextMonthForecastController from "./controller/NextMonthForecastController.jsx";
+// import ProductDemandForecastController from "./controller/ProductDemandForecastController.jsx";
+import ForecastGraphController from "./controller/ForecastGraphController.jsx";
+import InsightController from "./controller/InsightController.jsx"
+import ExportController from "./controller/ExportController.jsx";
+import SubHeader from "./views/SubHeader.jsx";
+
+const Forecast = () => {
+    return (
+        <>
+            <Header />
+            <SubHeader />
+            <div className="flex-col">
+                <div className="float-left w-1/3">
+                    <ForecastCardController />
+                    <NextMonthForecastController />
+                    {/*<ProductDemandForecastController />*/}
+                    <InsightController />
+                </div>
+                <div className="float-right w-2/3">
+                    <ForecastGraphController />
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Forecast;
